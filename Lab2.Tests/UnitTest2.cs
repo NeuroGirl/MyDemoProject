26f1bbdea6ef8ae15750ab72214bcce80b304b98
NeuroGirl <vesus0206@gmail.com>
2025-12-20 10:01:52 +0300

    Lab 2 version 0 tests fixed

--- Lab2.Tests/UnitTest2.cs
+++ Lab2.Tests/UnitTest2.cs
@@ -1,131 +1,135 @@
 ﻿using Game.Items;
 using Game.Equipping;
+using Game.Builder;  // Добавлено
+using Game.Factory; // Добавлено
+using Xunit;
 
-namespace Game.Tests;
+namespace Game.Tests
 {
-public class InventoryTests
-{
-    [Fact]
-    public void Add_Item_To_NormalState_ShouldAdd()
+    public class InventoryTests
     {
-        // Arrange
-        var inv = new Inventory.Inventory(2);
-        var sword = new Weapon("Sword", "A sharp blade", 10, new StandardEquipStrategy());
+        [Fact]
+        public void Add_Item_To_NormalState_ShouldAdd()
+        {
+            // Arrange
+            var inv = new Inventory.Inventory(2);
+            var sword = new Weapon("Sword", "A sharp blade", 10, new StandardEquipStrategy());
 
-        // Act
-        inv.Add(sword);
+            // Act
+            inv.Add(sword);
 
-        // Assert
-        Assert.Single(inv.Items);
-        Assert.Contains(sword, inv.Items);
-    }
+            // Assert
+            Assert.Single(inv.Items);
+            Assert.Contains(sword, inv.Items);
+        }
 
-    [Fact]
-    public void Add_Items_To_FullInventory_ShouldThrow()
-    {
-        // Arrange
-        var inv = new Inventory(1);
-        var sword = new Weapon("Sword", "A sharp blade", 10, new StandardEquipStrategy());
-        var shield = new Weapon("Shield", "Protective gear", 5, new StandardEquipStrategy());
-        inv.Add(sword);
+        [Fact]
+        public void Add_Items_To_FullInventory_ShouldThrow()
+        {
+            // Arrange
+            var inv = new Inventory.Inventory(1);
+            var sword = new Weapon("Sword", "A sharp blade", 10, new StandardEquipStrategy());
+            var shield = new Weapon("Shield", "Protective gear", 5, new StandardEquipStrategy());
+            inv.Add(sword);
 
-        // Act & Assert
-        Assert.Throws<InvalidOperationException>(() => inv.Add(shield));
-    }
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => inv.Add(shield));
+        }
 
-    [Fact]
-    public void Remove_Item_ShouldSucceed()
-    {
-        // Arrange
-        var inv = new Inventory(2);
-        var potion = new Potion("Healing", "Restores health", 20);
-        inv.Add(potion);
+        [Fact]
+        public void Remove_Item_ShouldSucceed()
+        {
+            // Arrange
+            var inv = new Inventory.Inventory(2);
+            var potion = new Potion("Healing", "Restores health", 20);
+            inv.Add(potion);
 
-        // Act
-        inv.Remove(potion);
+            // Act
+            inv.Remove(potion);
 
-        // Assert
-        Assert.Empty(inv.Items);
-    }
+            // Assert
+            Assert.Empty(inv.Items);
+        }
 
- [Fact]
-    public void BuildWeapon_ShouldReturnWeaponWithCorrectProperties()
-    {
-        // Arrange
-        var builder = new ItemBuilder();
+        [Fact]
+        public void BuildWeapon_ShouldReturnWeaponWithCorrectProperties()
+        {
+            // Arrange
+            var builder = new ItemBuilder();
 
-        // Act
-        var weapon = builder
-            .WithName("Excalibur")
-            .WithDescription("Legendary Sword")
-            .WithDamage(999)
-            .BuildWeapon();
+            // Act
+            var weapon = builder
+                .WithName("Excalibur")
+                .WithDescription("Legendary Sword")
+                .WithDamage(999)
+                .BuildWeapon();
 
-        // Assert
-        Assert.Equal("Excalibur", weapon.Name);
-        Assert.Equal(999, weapon.Damage);
-        Assert.False(weapon.IsEquipped);
-    }
+            // Assert
+            Assert.Equal("Excalibur", weapon.Name);
+            Assert.Equal(999, weapon.Damage);
+            Assert.False(weapon.IsEquipped);
+        }
 
-    [Fact]
-    public void BuildPotion_ShouldThrow_OnZeroHeal()
-    {
-        // Arrange
-        var builder = new ItemBuilder();
+        [Fact]
+        public void BuildPotion_ShouldThrow_OnZeroHeal()
+        {
+            // Arrange
+            var builder = new ItemBuilder();
 
-        // Act & Assert
-        Assert.Throws<InvalidOperationException>(() => builder.WithHeal(0).BuildPotion());
-    }
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => builder.WithHeal(0).BuildPotion());
+        }
 
-    [Fact]
-    public void EquipWeaponStrategy_ShouldMarkAsEquipped()
-    {
-        // Arrange
-        var builder = new ItemBuilder();
-        var weapon = builder
-            .WithName("Dagger")
-            .WithDamage(20)
-            .WithEquipStrategy(EquipWeaponStrategy.Default)
-            .BuildWeapon();
+        [Fact]
+        public void EquipWeaponStrategy_ShouldMarkAsEquipped()
+        {
+            // Arrange
+            var builder = new ItemBuilder();
+            var weapon = builder
+                .WithName("Dagger")
+                .WithDamage(20)
+                .WithEquipStrategy(EquipWeaponStrategy.Default)
+                .BuildWeapon();
 
-        // Act
-        weapon.Equip();
+            // Act
+            weapon.Equip();
 
-        // Assert
-        Assert.True(weapon.IsEquipped);
-    }
+            // Assert
+            Assert.True(weapon.IsEquipped);
+        }
 
-    [Fact]
-    public void StandardFactory_ShouldProduceItems_WithCorrectType()
-    {
-        // Arrange
-        var factory = new StandardFactory();
+        [Fact]
+        public void StandardFactory_ShouldProduceItems_WithCorrectType()
+        {
+            // Arrange
+            var factory = new StandardFactory();
 
-        // Act
-        var weapon = factory.CreateWeaponFactory().CreateWeapon("Axe", 15);
-        var armor = factory.CreateArmorFactory().CreateArmor("Chainmail", 25);
-        var potion = factory.CreatePotionFactory().CreatePotion("Mana", 30);
-        var quest = factory.CreateQuestFactory().CreateQuestItem("Key");
+            // Act
+            var weapon = factory.CreateWeaponFactory().CreateWeapon("Axe", 15);
+            var armor = factory.CreateArmorFactory().CreateArmor("Chainmail", 25);
+            var potion = factory.CreatePotionFactory().CreatePotion("Mana", 30);
+            var quest = factory.CreateQuestFactory().CreateQuestItem("Key");
 
-        // Assert
-        Assert.IsType<Weapon>(weapon);
-        Assert.IsType<Armor>(armor);
-        Assert.IsType<Potion>(potion);
-        Assert.IsType<QuestItem>(quest);
-    }
+            // Assert
+            Assert.IsType<Weapon>(weapon);
+            Assert.IsType<Armor>(armor);
+            Assert.IsType<Potion>(potion);
+            Assert.IsType<QuestItem>(quest);
+        }
 
-    [Fact]
-    public void EquippingStrategy_Assigned_Correctly()
-    {
-        // Arrange
-        var factory = new StandardFactory();
-        var weaponFactory = factory.CreateWeaponFactory();
+        [Fact]
+        public void EquippingStrategy_Assigned_Correctly()
+        {
+            // Arrange
+            var factory = new StandardFactory();
+            var weaponFactory = factory.CreateWeaponFactory();
 
-        // Act
-        var weapon = weaponFactory.CreateWeapon("Hammer", 12);
-        weapon.Equip();
+            // Act
+            var weapon = weaponFactory.CreateWeapon("Hammer", 12);
+            weapon.Equip();
 
-        // Assert
-        Assert.True(weapon.IsEquipped);
+            // Assert
+            Assert.True(weapon.IsEquipped);
+        }
     }
 }
--- obj/Debug/net9.0/MyDemoProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyDemoProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyDemoProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9b1996ec24eb44b73afeb0bc4b3eadf01b5b6d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b76097b4f9518da33210faaa6a37d8698d265eb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyDemoProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyDemoProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
